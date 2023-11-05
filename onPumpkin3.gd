03c234c02b28574699f6d87c90c67c9a28284581
SuperBro1002 <126834262+SuperBro1002@users.noreply.github.com>
2023-11-05 11:58:39 -0800

    Applied Assets to nodes

--- onPumpkin3.gd
+++ onPumpkin3.gd
@@ -6,6 +6,7 @@ var state = 1
 func _ready():
 	show()
 	$CollisionShape2D.set_deferred("disabled", false)
+	$CollisionShape2D2.set_deferred("disabled", false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,8 +18,10 @@ func _on_timer_timeout():
 	if state == 1:
 		hide()
 		$CollisionShape2D.set_deferred("disabled", true)
+		$CollisionShape2D2.set_deferred("disabled", true)
 		state = 0
 	else:
 		show()
 		$CollisionShape2D.set_deferred("disabled", false)
+		$CollisionShape2D2.set_deferred("disabled", false)
 		state = 1
